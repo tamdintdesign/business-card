d13f004ce1c52298b24cae9157f5712456d44fc3
fentra <growth4uwb@icloud.com>
2026-04-16 14:46:03 +0100

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/YOUR_REPO_NAME/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/business-card/' : '/',
 })
